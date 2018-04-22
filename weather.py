f39e1fb05239e2fda059f808bab822e443953fb0
quickbrownfox319 <quickbrownfox319@gmail.com>
2018-04-22 06:23:35 -0400

    fixed up weather to be an array

--- weather.py
+++ weather.py
@@ -5,27 +5,22 @@ import json
 #import face.face_recog as test
 
 #wunderground API
-def get_weather(zip_code):
+def get_weather():
 
     with open('creds.yml', 'r') as txt:
         key = yaml.load(txt)
 
     key = key['wunderground']
-    url = 'http://api.wunderground.com/api/{}/geolookup/forecast/conditions/q/{}.json'.format(key,zip_code)
+    url = 'http://api.wunderground.com/api/{}/hourly/forecast/q/NY/NewYork.json'.format(key)
     r = requests.get(url)
-    # f = urllib2.urlopen(url)
     
     json_string = r.text
     parsed_json = json.loads(json_string)
-    location = parsed_json['location']['city']
 
-    # #next day's forecast
     forecast_prefix = parsed_json['forecast']['txt_forecast']['forecastday'][2]
-    day = forecast_prefix['title']
-    forecast = forecast_prefix['fcttext']
-
+    forecast = str(forecast_prefix['fcttext_metric']).split(".")
     forecast_msg = (forecast)
     return forecast_msg
 
 #test
-print(get_weather('07922'))
+print(get_weather())
